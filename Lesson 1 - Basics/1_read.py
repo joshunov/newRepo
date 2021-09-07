ae195a68f43f8c01af349bbce8ba9d0b2377b4df
joshunov <joshnov7@gmail.com>
2021-09-07 14:10:27 +1000

    s
    s

---
+++ Lesson 1 - Basics/1_read.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy
+
+#reading images
+img = cv2.imread('Photos/cat.jpg')
+cv2.imshow('Cat', img)
+
+<<<<<<< HEAD
+
+=======
+'''
+>>>>>>> 9832787dd7feae66a9d74935a9286f6bf50e3f14
+# reading videos
+
+capture = cv2.VideoCapture('..\Videos\dog.mp4')
+#VideoCapture(0) = Webcam input
+
+#capture=cv2.VideoCapture(0)
+
+while True:
+    isTrue,frame=capture.read()
+
+    #width,height = frame.size()
+
+
+    cv2.circle(frame,(500,500),40,(255,0,0),thickness=2)
+    cv2.imshow('Circle',frame)
+
+
+    #show frame
+    cv2.imshow('',frame)
+
+    #if the d key is pressed, kill screen
+    if cv2.waitKey(20) & 0xFF==ord('d'):
+        break
+'''
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
