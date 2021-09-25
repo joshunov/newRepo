cc5fd09e8b5acaa3235715c932a1acfba1c77141
joshunov <joshnov7@gmail.com>
2021-09-25 12:01:15 +1000

    put in photos

---
+++ Week6_challenge.py
@@ -0,0 +1,13 @@
+'''
+## Lesson 3 - Challenge
+- Consider `Faces/usrc_all.png` and `Faces/usrc_cropped.png`. 
+Using the face detection in part 1, tune the face detection so that all the faces 
+in the cropped version are accounted for and there are no extra faces in the uncropped 
+version. (Except for the clock. Apparently the clock looks like a face.)
+
+- For each detected face (including the clock, I guess) use the imageFrame in 
+`Faces/imageframe.png` to enframe the face, putting the resultant images into 
+an output folder.
+
+'''
+
